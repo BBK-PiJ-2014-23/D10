d6fa70ab8ee6d672500841811cc8aa4dd5572604
unknown <smayer04@CSPC060.dcs.bbk.ac.uk>
2014-12-01 19:45:12 +0000

    Fist commit.

---
+++ ExtensionExtension/MobilePhone.java
@@ -0,0 +1,30 @@
+public class MobilePhone extends OldPhone {
+    public String[] lastNumbers;
+
+    public MobilePhone() {
+        lastNumbers = new String[10];
+    }
+
+    public void ringAlarm(String alarm) {
+        System.out.println("Ringing alarm " + alarm + "...");
+    }
+
+    public void playGame(String game) {
+        System.out.println("Playing game " + game + "...");
+    }
+
+    public void printLastNumbers() {
+        for (int i = 0; i < lastNumbers.length; i++) {
+            System.out.println(lastNumbers[i]);
+        }
+    }
+    
+    @Override
+    public void call(String number) {
+        super.call(number);
+        for (int i = lastNumbers.length - 1; i > 0; i--) {
+                lastNumbers[i] = lastNumbers[i - 1];
+        }
+        lastNumbers[0] = number;
+    }
+}
